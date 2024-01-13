082d5ccbab364ae03cd48cbb3c5533ed498ac739
banuka <banukaramanayaka76@gmail.com>
2024-01-13 17:58:34 +0530

    feat:all books scrapaed

--- books/books/spiders/bookspider.py
+++ books/books/spiders/bookspider.py
@@ -8,4 +8,20 @@ class BookscraperSpider(scrapy.Spider):
 
 # parse get called when the response comes back
     def parse(self, response):
-        pass
+        books=response.css('article.product_pod')
+        for book in books:
+            yield{
+                'name':book.css('h3 a::text').get(),
+                'price':book.css('div.product_price p::text').get(),
+                'url':book.css('h3 a').attrib['href']
+            }
+        
+        nextPage=response.css('li.next a::attr("href")').get()
+        if nextPage is not None:
+            if 'catalogue/'  in nextPage:
+                nextPageUrl=f"https://books.toscrape.com/{nextPage}"
+            else:
+                nextPageUrl=f"https://books.toscrape.com/catalogue/{nextPage}"
+
+
+            yield response.follow(nextPageUrl,callback=self.parse)
